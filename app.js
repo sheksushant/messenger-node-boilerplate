2c25ffcfadff584cbeb9506de682e8fdfe3658be
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-08-07 03:44:34 +0530

    verify app

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+// const others = require("./others")
+
+app.set('port', (process.env.PORT || 5000))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// index
+app.get('/', function (req, res) {
+	res.send('HELLO WORLD!')
+})
+
+// used for facebook auth
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'secret_key') {
+		res.send(req.query['hub.challenge'])
+	} else {
+		res.send('Error, wrong token')
+	}
+})
+
+// to post data back
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+// recommended to inject access tokens as environmental variables, e.g.
+// const token = process.env.FB_PAGE_ACCESS_TOKEN
+const token = "PAGE_ACCESS_TOKEN"
+// spin spin sugar
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
